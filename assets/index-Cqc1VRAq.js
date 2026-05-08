97c6faefac54c7cd27af09dd22cea8e550c48443
devdio <iamtopaz@gmail.com>
2026-05-08 14:53:04 +0900

    Fix sub-path 404: use relative path for pyodide-descriptions.json

--- assets/index-Cqc1VRAq.js
+++ assets/index-Cqc1VRAq.js
@@ -1,4 +1,4 @@
-import{f as ee,g as ue}from"./index-znWky3IC.js";import{d as le,r as ce}from"./index-5ZTswUa_.js";function fe(i,g){for(var _=0;_<g.length;_++){const o=g[_];if(typeof o!="string"&&!Array.isArray(o)){for(const A in o)if(A!=="default"&&!(A in i)){const S=Object.getOwnPropertyDescriptor(o,A);S&&Object.defineProperty(i,A,S.get?S:{enumerable:!0,get:()=>o[A]})}}}return Object.freeze(Object.defineProperty(i,Symbol.toStringTag,{value:"Module"}))}var U={},q={};const k=ee(le);var V={},X;function te(){if(X)return V;X=1;/**
+import{f as ee,g as ue}from"./index-D7gnnIAs.js";import{d as le,r as ce}from"./index-B0Z3FUvA.js";function fe(i,g){for(var _=0;_<g.length;_++){const o=g[_];if(typeof o!="string"&&!Array.isArray(o)){for(const A in o)if(A!=="default"&&!(A in i)){const S=Object.getOwnPropertyDescriptor(o,A);S&&Object.defineProperty(i,A,S.get?S:{enumerable:!0,get:()=>o[A]})}}}return Object.freeze(Object.defineProperty(i,Symbol.toStringTag,{value:"Module"}))}var U={},q={};const k=ee(le);var V={},X;function te(){if(X)return V;X=1;/**
  * @license
  * Copyright 2019 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
--- assets/python-DpcOwzrw.js
+++ assets/python-DpcOwzrw.js
@@ -1,4 +1,4 @@
-import{m as o}from"./monacoBundle-C30Fviy5.js";import"./index-znWky3IC.js";/*!-----------------------------------------------------------------------------
+import{m as o}from"./monacoBundle-bFGJH8nm.js";import"./index-D7gnnIAs.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
